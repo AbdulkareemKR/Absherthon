646008d9ab650d2d0fc9c30448a3f31f8ca582b7
abdulkareemkr <abdulkarim.k.r11@gmail.com>
2021-11-08 13:43:01 +0300

    icons and numeric adding

--- client/src/component/Registration.js
+++ client/src/component/Registration.js
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Fade } from "react-awesome-reveal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Axios from "axios";
 import "../App.css";
 import Spinner from "react-bootstrap/Spinner";
+import styles from "./main.module.css";
+import Modal from "react-bootstrap/Modal";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Col, Row, Form } from "react-bootstrap";
+import { FaIdCard } from "react-icons/fa";
+import { BsFillPeopleFill } from "react-icons/bs";
+import { HiTag } from "react-icons/hi";
+import { MdEmail } from "react-icons/md";
+import { AiFillUnlock } from "react-icons/ai";
+import Container from "react-bootstrap/Container";
+import NumericInput from "react-numeric-input";
 
 function Registration() {
   const [movieName, setMovieName] = useState("");
@@ -14,11 +23,6 @@ function Registration() {
   const [movieReviewList, setMovieReviewList] = useState([]);
   const [newReview, setNewReview] = useState("");
   const [edit, setEdit] = useState(false);
-  const [loading, setLoading] = useState(true);
-
-  const editMovie = () => {
-    setEdit(!edit);
-  };
 
   const submitReview = () => {
     Axios.post("https://crud-back-end-node.herokuapp.com/api/insert", {
@@ -33,134 +37,113 @@ function Registration() {
     ]);
   };
 
-  const deleteReview = (movie) => {
-    Axios.delete(
-      `https://crud-back-end-node.herokuapp.com/api/delete/${movie}`
-    );
-  };
-
-  const updateReview = (movie) => {
-    Axios.put("https://crud-back-end-node.herokuapp.com/api/update", {
-      movieName: movie,
-      movieReview: newReview,
-    });
-    setNewReview("");
-  };
-
-  useEffect(() => {
-    setTimeout(setLoading, 1000, false);
-    //setLoading(false);
-  }, [movieReviewList]);
-
-  useEffect(() => {
-    Axios.get("https://crud-back-end-node.herokuapp.com/api/get").then(
-      (response) => {
-        setMovieReviewList(response.data);
-      }
-    );
-  }, [movieReviewList]);
+  // const updateReview = (movie) => {
+  //   Axios.put("https://crud-back-end-node.herokuapp.com/api/update", {
+  //     movieName: movie,
+  //     movieReview: newReview,
+  //   });
+  //   setNewReview("");
+  // };
 
   return (
     <div>
-      hehe
-      {/* <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your name"
-            onChange={(e) => {
-              setMovieName(e.target.value);
-            }}
-            value={movieName}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter your description"
-            onChange={(e) => {
-              setReview(e.target.value);
-            }}
-            value={review}
-          />
-        </Form.Group>
-        <Button onClick={submitReview}>Submit</Button>
-        <br />
-        <div className="wrapper">
-          {loading ? (
-            <Spinner
-              animation="grow"
-              className="spinner1"
-              // style={{ color: "red" }}
-            />
-          ) : (
-            <div>
-              {movieReviewList.map((value) => {
-                return (
-                  <div>
-                    <Fade
-                      durtion={1200}
-                      cascade
-                      damping={0.02}
-                      triggerOnce // to present each element on itself while moving down
-                      direction="up"
-                    >
-                      <Card className="text-center">
-                        <Card.Header style={{ margin: "auto" }}>
-                          Information Card
-                        </Card.Header>
-                        <Card.Body>
-                          <Card.Title>
-                            {edit ? (
-                              <Form.Control
-                                type="text"
-                                defaultValue={value.name}
-                                onChange={(e) => {
-                                  setNewReview(e.target.value);
-                                }}
-                              />
-                            ) : (
-                              <div>{value.name}</div>
-                            )}
-                          </Card.Title>
-                          <Card.Text>{value.review}</Card.Text>
-                          <Button
-                            variant="danger"
-                            onClick={() => {
-                              deleteReview(value.name);
-                            }}
-                          >
-                            Delete
-                          </Button>
-                          <div>{newReview}</div>
-                          <Button
-                            variant="primary"
-                            onClick={() => {
-                              updateReview(value.name);
-                            }}
-                          >
-                            Change
-                          </Button>
-
-                          <Button variant="primary" onClick={editMovie}>
-                            Edit
-                          </Button>
-                        </Card.Body>
-                        <div>
-                          <br />
-                        </div>
-                      </Card>
-                    </Fade>
-                  </div>
-                );
-              })}
-            </div>
-          )}
+      <div>
+        <div className={styles.containter}>
+          <Modal.Header className={styles.title}>
+            <Modal.Title
+              className={styles.title}
+              id="contained-modal-title-vcenter"
+            >
+              استعلام
+              <BsFillPeopleFill className={styles.icons} />
+            </Modal.Title>
+          </Modal.Header>
+          <Form className={styles.formStyle}>
+            <Container fluid style={{ margin: "auto" }}>
+              <Modal.Body className="show-grid">
+                <Form.Group as={Row} className={styles.group}>
+                  <Form.Label className={styles.label} column sm="4">
+                    <FaIdCard className={styles.icons} /> الاسم
+                  </Form.Label>
+                  <Col>
+                    <Form.Control
+                      required
+                      className={styles.input}
+                      type="number"
+                      placeholder="أدخل اسمك"
+                    />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} className={styles.group}>
+                  <Form.Label className={styles.label} column sm="4">
+                    <MdEmail className={styles.icons} /> الايميل
+                  </Form.Label>
+                  <Col>
+                    <Form.Control
+                      required
+                      className={styles.input}
+                      type="email"
+                      placeholder="أدخل ايميلك"
+                    />
+                  </Col>
+                </Form.Group>
+                <Form.Group as={Row} className={styles.group}>
+                  <Form.Label className={styles.label} column sm="4">
+                    <AiFillUnlock className={styles.icons} /> كلمة المرور
+                  </Form.Label>
+                  <Col>
+                    <Form.Control
+                      required
+                      className={styles.input}
+                      type="password"
+                      placeholder="أدخل كلمة المرور"
+                    />
+                  </Col>
+                </Form.Group>
+                <div>
+                  <Form.Group as={Row} className={styles.group}>
+                    <Form.Label className={styles.label} column sm="4">
+                      <AiFillUnlock className={styles.icons} />
+                      تأكيد كلمة المرور
+                    </Form.Label>
+                    <Col>
+                      <Form.Control
+                        required
+                        className={styles.input}
+                        type="password"
+                        placeholder="أعد ادخال كلمة المرور"
+                      />
+                    </Col>
+                  </Form.Group>
+                  <Form.Group as={Row} className={styles.group}>
+                    <Form.Label className={styles.label} column sm="4">
+                      <HiTag className={styles.icons} /> المرحلة الدراسية
+                    </Form.Label>
+                    <Col>
+                      <Form.Control as="select" className={styles.input}>
+                        <option>دون الابتدائي</option>
+                        <option>ابتدائي</option>
+                        <option>متوسط</option>
+                        <option>ثانوي</option>
+                        <option>جامعي</option>
+                        <option>أب</option>
+                      </Form.Control>
+                    </Col>
+                  </Form.Group>
+                </div>
+              </Modal.Body>
+              <Modal.Footer className={styles.footer}>
+                <Button
+                  className={`${styles.createButton} ${styles.submit}`}
+                  type="submit"
+                >
+                  أنشئ الحساب
+                </Button>
+              </Modal.Footer>
+            </Container>
+          </Form>
         </div>
-      </Form> */}
+      </div>
     </div>
   );
 }
